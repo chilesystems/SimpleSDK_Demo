21f03810ab0f1ee64e91d7b33079f281dca4f348
Felipe Cerda Saavedra <felipecerdasaavedra@gmail.com>
2022-06-08 14:42:53 -0400

    Formulario de envio boleta por email llama a la función correspondiente en SimpleSDK

--- EnviarBoletaPorEmail.Designer.cs
+++ EnviarBoletaPorEmail.Designer.cs
@@ -39,11 +39,15 @@ namespace SimpleSDK_Demo
             this.label1 = new System.Windows.Forms.Label();
             this.RutEmisorTextbox = new System.Windows.Forms.TextBox();
             this.EnviarEmailButton = new System.Windows.Forms.Button();
+            this.CorreoLabel = new System.Windows.Forms.Label();
+            this.CorreoTextBox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.CorreoTextBox);
+            this.groupBox1.Controls.Add(this.CorreoLabel);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.AnioTextbox);
             this.groupBox1.Controls.Add(this.label2);
@@ -53,7 +57,7 @@ namespace SimpleSDK_Demo
             this.groupBox1.Controls.Add(this.EnviarEmailButton);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(722, 113);
+            this.groupBox1.Size = new System.Drawing.Size(1095, 113);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos de la Boleta de Honorarios Electrónica";
@@ -108,25 +112,47 @@ namespace SimpleSDK_Demo
             // 
             // EnviarEmailButton
             // 
-            this.EnviarEmailButton.Location = new System.Drawing.Point(597, 28);
+            this.EnviarEmailButton.Location = new System.Drawing.Point(969, 25);
             this.EnviarEmailButton.Name = "EnviarEmailButton";
             this.EnviarEmailButton.Size = new System.Drawing.Size(110, 66);
             this.EnviarEmailButton.TabIndex = 0;
             this.EnviarEmailButton.Text = "Enviar Email";
             this.EnviarEmailButton.UseVisualStyleBackColor = true;
+            this.EnviarEmailButton.Click += new System.EventHandler(this.EnviarEmailButton_Click);
+            // 
+            // CorreoLabel
+            // 
+            this.CorreoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CorreoLabel.Location = new System.Drawing.Point(562, 48);
+            this.CorreoLabel.Name = "CorreoLabel";
+            this.CorreoLabel.Size = new System.Drawing.Size(71, 23);
+            this.CorreoLabel.TabIndex = 7;
+            this.CorreoLabel.Text = "Correo";
+            // 
+            // CorreoTextBox
+            // 
+            this.CorreoTextBox.Location = new System.Drawing.Point(639, 45);
+            this.CorreoTextBox.Name = "CorreoTextBox";
+            this.CorreoTextBox.Size = new System.Drawing.Size(311, 26);
+            this.CorreoTextBox.TabIndex = 8;
             // 
             // EnviarBoletaPorEmail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(749, 140);
+            this.ClientSize = new System.Drawing.Size(1119, 140);
             this.Controls.Add(this.groupBox1);
             this.Name = "EnviarBoletaPorEmail";
             this.Text = "Enviar Boleta de Honorarios por Email";
+            this.Load += new System.EventHandler(this.EnviarBoletaPorEmail_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.TextBox CorreoTextBox;
+
+        private System.Windows.Forms.Label CorreoLabel;
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label3;
--- EnviarBoletaPorEmail.cs
+++ EnviarBoletaPorEmail.cs
@@ -1,12 +1,64 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using SimpleSDK.Helpers;
+using SimpleSDK.Models.BHE;
+using SimpleSDK_Demo.Models;
 
 namespace SimpleSDK_Demo
 {
     public partial class EnviarBoletaPorEmail : Form
     {
+        Helper handler = new Helper();
         public EnviarBoletaPorEmail()
         {
             InitializeComponent();
+        }
+
+        private void EnviarBoletaPorEmail_Load(object sender, EventArgs e)
+        {
+            handler.Configuracion = new Configuracion();
+            handler.Configuracion.LeerArchivo();
+            RutEmisorTextbox.Text = handler.Configuracion.Certificado.Rut;
+            RutEmisorTextbox.Enabled = false;
+        }
+
+        private async void EnviarEmailButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var folio = int.Parse(FolioTextbox.Text);
+                var rutaCertificado = handler.Configuracion.Certificado.Ruta;
+                var certificado = System.IO.File.ReadAllBytes(rutaCertificado);
+                var rutUsuario = handler.Configuracion.Certificado.Rut;
+                var password = handler.Configuracion.Certificado.Rut;
+                var apikey = handler.Configuracion.APIKey;
+                var correo = CorreoTextBox.Text;
+                var anio = int.Parse(AnioTextbox.Text);
+                var input = new BHData
+                {
+                    RutUsuario = rutUsuario,
+                    Password = password,
+                    CertificadoB64 = certificado,
+                    Correo = correo
+                };
+                var (boletaEnviada, message) = await BHHelper.EnviarEmail(input, folio, apikey, anio);
+                if (boletaEnviada)
+                {
+                    var buttons = MessageBoxButtons.OK;
+                    var messageBoxMessage = "Se envió la boleta de honorarios electrónica al correo indicado";
+                    var caption = "Envío de Boletas de Honorarios";
+                    MessageBox.Show(messageBoxMessage, caption, buttons);
+                }
+                else
+                {
+                    Console.WriteLine(message);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
         }
     }
 }
